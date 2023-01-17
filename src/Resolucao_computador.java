a7a4be8fea0b2bc9cc4b4045297eb9b77358d5d9
kaique <kaique.dultra@outlook.com>
2023-01-17 15:09:40 -0300

    exercicio 02

--- src/Resolucao_computador.java
+++ src/Resolucao_computador.java
@@ -4,8 +4,8 @@ public class Resolucao_computador {
     public static void main(String[] args){
         //Resolucao do computador
         Dimension tamanho = Toolkit.getDefaultToolkit().getScreenSize(); //GetScreenSize retorna o tamanho da tela do dispositivo
-        int width = (int) tamanho.getWidth();
-        int height = (int) tamanho.getHeight();
+        int width = (int) tamanho.getWidth(); //Buscando tamanho do width
+        int height = (int) tamanho.getHeight(); //Buscando tamanho do height
         System.out.println("O tamanho da dimensão da tela do seu computador é: ");
         System.out.println("Width: " + width + " Height: " + height);
     }
