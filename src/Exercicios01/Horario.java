90907804282b0fdaf9202f10379f5a6823108a76
kaique <kaique.dultra@outlook.com>
2023-01-18 08:28:51 -0300

    Exercicio01

--- src/Exercicios01/Horario.java
+++ src/Exercicios01/Horario.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Horario {
     public static void main(String[] args) {
-        //Buscando o horário do cumputador
+        //Buscando o horário do sistema do cumputador
         Date relogio = new Date();
         System.out.println("O horário do seu computador é: " + relogio.toString());
     }
--- src/Exercicios01/Idioma.java
+++ src/Exercicios01/Idioma.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 public class Idioma {
     public static void main(String[] args){
-        //Idioma do sistema
+        //Idioma do sistema operacional
         Locale idioma = Locale.getDefault(); //Buscando o idioma do dispositivo
         String linguagem = idioma.getDisplayLanguage(); //Armazenando o idioma na string linguagem
         System.out.println("O idioma do seu computador é: " + linguagem);
